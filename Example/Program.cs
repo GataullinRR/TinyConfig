4a451c47086c8329baa184d432d50765fa8f4e42
Radmir <blabla@lala.com>
2018-05-06 16:05:34 +0500

    Добавлен пример использования

---
+++ Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyConfig;
+
+namespace Example
+{
+    class Program
+    {
+        static readonly ConfigAccessor _config = Configurable.CreateConfig("ExampleConfigFile");
+        static readonly ConfigProxy<int> SOME_INT = _config.ReadValue(100);
+        static readonly ConfigProxy<string> SOME_MULTILINE_STRING_INT = _config.ReadValue("'Hello' \n\r all!");
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"SOME_INT: {SOME_INT}");
+            Console.WriteLine($"SOME_MULTILINE_STRING_INT: {SOME_MULTILINE_STRING_INT}");
+            SOME_MULTILINE_STRING_INT.Value = "New value";
+            Console.WriteLine($"SOME_MULTILINE_STRING_INT(changed): {SOME_MULTILINE_STRING_INT}");
+
+            Console.ReadKey();
+        }
+    }
+}
